c9b827aa0fbaa7e611855265f5f49826cc63eda8
Karl Mantle <130184376+karl-mantle@users.noreply.github.com>
2024-10-21 19:12:50 +0100

    added github actions for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://karl-mantle.github.io/',
+    base: 'astro-headless-wp-example',
+});
