86250050ae4df9a3aa41fdc1d6815615e6ab012a
Karl Mantle <130184376+karl-mantle@users.noreply.github.com>
2024-10-21 19:18:08 +0100

    updated config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-    site: 'https://karl-mantle.github.io/',
-    base: 'astro-headless-wp-example',
-});
+export default defineConfig({});
